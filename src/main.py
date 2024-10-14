30d9d663ce28268e7603e588aa446805f0674fb7
Deic1da <melquenerd@gmail.com>
2024-10-14 16:56:54 -0300

    tela de enceramento adicionada

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ from pages import *
 from pages.inicial_page import inicial_page
 from services.criar_tabelas import criar_tabelas
 from services.verificador import verificar_funcionario
+import time
+import os
 
 criar_tabelas()
 
@@ -18,10 +20,22 @@ try:
             funcionario = login_page()
             if funcionario is not None:
                 break
-    #funcionario = 1
+    
     inicial_page(funcionario)
 
 except Exception as e:
-    print(f"\033[91mErro inesperado: {e}\033[0m")
+    print(f"\n\033[91mErro inesperado: {e}\033[0m")
+    time.sleep(2)
+    inicial_page()
 finally:
-    print("Desligando...")
+    os.system("cls")
+    print("\033[93mDesligando[10%]... Preparando para desligar\033[0m")
+    time.sleep(0.5)
+    print("\033[93mDesligando[30%]... Salvando progresso\033[0m")
+    time.sleep(0.8)
+    print("\033[93mDesligando[50%]... Finalizando processos\033[0m")
+    time.sleep(1)
+    print("\033[93mDesligando[80%]... Limpando memória\033[0m")
+    time.sleep(1.2)
+    print("\033[93mDesligando[100%]... Sistema desligado com sucesso!\033[0m")
+    time.sleep(2)
